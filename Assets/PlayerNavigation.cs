cad4f6c558534a7dd3bd364720e1a37b44adf1f6
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2021-03-19 16:44:41 +0100

    basic player navigation

---
+++ Assets/PlayerNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavigation : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private Camera mainCamera = null;
+    private const float MaxRayDistance = 100f;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, MaxRayDistance))
+            return;
+
+        agent.SetDestination(hit.point);
+    }
+}
