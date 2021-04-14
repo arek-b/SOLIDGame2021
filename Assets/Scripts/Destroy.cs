6e89b688d9529d39725ac82971ac9b38481035df
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-04-14 14:24:50 +0200

    Object Pooling Testing
    For enemy swarm (rodents puzzle).

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public float _time = 1.35f;
+    public GameObject _object;
+    void Start()
+    {
+        Destroy(_object, _time);
+    }
+}
---
+++ Assets/Scripts/SwarmSpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmSpawn : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _swarmPool;
+    [SerializeField] private GameObject _swarmContainmentUnit;
+    [SerializeField] private GameObject _swarmUnit;
+    [SerializeField] private Transform _swarmSpawn;
+    private GameObject _rodent; 
+    private GameObject _rodentRequester;
+    private void Start()
+    {
+        _swarmPool = GenerateSwarm(30);
+        _rodentRequester = RequestPrefab();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            RequestPrefab();
+            _rodent.transform.position = _swarmSpawn.position;
+            _rodent.transform.rotation = _swarmSpawn.rotation;
+            /*
+            _rodent = Instantiate(_swarmUnit) as GameObject;
+            _rodent.transform.position = _swarmSpawn.position;
+            _rodent.transform.rotation = _swarmSpawn.rotation;
+            */
+        }
+    }
+    List <GameObject> GenerateSwarm(int p_amountOfSwarm)
+    {
+        for(int i = 0; i < p_amountOfSwarm; i++)
+        {
+            GameObject rodent = Instantiate(_swarmUnit);
+            rodent.transform.parent = _swarmContainmentUnit.transform;
+            rodent.SetActive(false);
+            _swarmPool.Add(rodent);
+        }
+        return _swarmPool;
+    }
+    public GameObject RequestPrefab()
+    {
+        foreach(var prefab in _swarmPool)
+        {
+            if(prefab.activeInHierarchy == false)
+            {
+                prefab.SetActive(true);
+                return prefab;
+            }
+        }
+        _swarmPool = GenerateSwarm(1);
+        GameObject newPrefab = _swarmPool[_swarmPool.Count - 1];
+        newPrefab.SetActive(true);
+        return newPrefab;
+    }
+}
