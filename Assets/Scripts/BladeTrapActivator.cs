3b7846ef99e84c06e17397da67a1b7826d6252db
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-05-05 23:06:02 +0200

    Angry Blades + New Textures + New Level
    All new, all shiny, all spectacular and still not working properly! Yeah!

---
+++ Assets/Scripts/BladeTrapActivator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrapActivator : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _blades;
+    [SerializeField] private float _cutInterval = 0.2f;
+    private int _bladeNum;
+    void Start()
+    {
+        _bladeNum = _blades.Count; 
+        StartCoroutine(ActivateBlade(_blades.Count-1));
+    }
+    void Another(int bladeNum) 
+    {
+        StartCoroutine(ActivateBlade(bladeNum));
+        /*for (int i = 0; i < _blades.Count; i++)
+        {
+            StartCoroutine(ActivateBlade(i));
+        }*/
+    }
+    IEnumerator ActivateBlade(int bladeNumber)
+    {
+        yield return new WaitForSeconds(_cutInterval);
+        if (bladeNumber >= 0)
+        {
+            _blades[bladeNumber].GetComponent<Animator>().enabled = true;
+            _bladeNum--;
+            Another(_bladeNum);
+        }
+    }
+}
