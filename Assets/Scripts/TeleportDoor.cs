f10d7c71bc504409cae3068448d6119cd5d10841
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-05-20 16:03:44 +0200

    Castle decor + torch setup + teleport attempt
    Teleport not working properly. Probably agent destination has to be reset.

--- Assets/Scripts/TeleportDoor.cs
+++ Assets/Scripts/TeleportDoor.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class TeleportDoor : MonoBehaviour
 {
-    private Transform _playerTransform;
+    [SerializeField] private Transform _playerTransform;
     [SerializeField] private Transform _entryPoint;
     [SerializeField] private Transform _exitPoint;
     [SerializeField] private GameObject _enteringCanvasObj;
     [SerializeField] private GameObject _exitingCanvasObj;
+    [SerializeField] private GameObject _obstacleWall;
     [SerializeField] private float _teleportingTime = 2f;
     void OnTriggerEnter(Collider other)
     {
         PlayerModelCollider player = other.GetComponent<PlayerModelCollider>();
         if (player != null)
         {
-            _playerTransform = other.transform;
+            _playerTransform = player.Player.transform; 
+            //_playerTransform = other.transform;
             _enteringCanvasObj.SetActive(true);
+            _obstacleWall.SetActive(false);
+            _playerTransform.transform.position = _exitPoint.transform.position;
             StartCoroutine(Teleport());
         }
     }
     IEnumerator Teleport()
     {
-        _playerTransform = _entryPoint;
+        //_playerTransform = _entryPoint;
+        _obstacleWall.SetActive(true);
         yield return new WaitForSeconds(_teleportingTime);
         _enteringCanvasObj.SetActive(false);
         _exitingCanvasObj.GetComponent<Animator>().SetTrigger("Fade");
-        _playerTransform = _exitPoint;
     }
 }
