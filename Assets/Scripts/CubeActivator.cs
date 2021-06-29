b006e8fee96489fed6f74b906dd35fffc88203de
Janko459 <57016587+Janko459@users.noreply.github.com>
2021-06-29 22:44:01 +0200

    Working game (almost)
    Added all puzzles up to level 10. Cubes not working yet.

---
+++ Assets/Scripts/CubeActivator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActivator : MonoBehaviour
+{
+    [SerializeField] private int _cubeNumber = 0;
+    [SerializeField] private GameObject _puzzleGlow;
+    private bool _activated = false;
+    private bool _deactivated = false;
+    private Player _playerScr;
+    [SerializeField] private GameObject _puzzlePiece;
+    private Puzzle _puzzle;
+    private void Start()
+    {
+        _puzzle = _puzzlePiece.GetComponent<Puzzle>();
+    }
+    void Update()
+    {
+        if (_activated == true)
+        {
+            if (_playerScr != null && _playerScr.Death.IsDead)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                _playerScr.GetComponent<PlayerCubeCollection>().GetACube(_cubeNumber);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (_deactivated == false)
+        {
+            PlayerModelCollider playerModelCollider = other.GetComponent<PlayerModelCollider>();
+            if (playerModelCollider != null)
+            {
+                _playerScr = playerModelCollider.Player;
+                Activate();
+            }
+        }
+    }
+    void Activate()
+    {
+        _puzzleGlow.SetActive(true);
+        _activated = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (_deactivated == false)
+        {
+            if (other.GetComponent<PlayerModelCollider>())
+            {
+                _activated = false;
+                _puzzleGlow.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCubeCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCubeCollection : MonoBehaviour
+{
+    [SerializeField] private List<bool> _cubes;
+    public void GetACube(int cubeNumber)
+    {
+        _cubes[cubeNumber] = true;
+    }
+}
